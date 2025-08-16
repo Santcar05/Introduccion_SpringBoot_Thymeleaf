a24347b6f1ff1c5073c24fb1ee48ae3da605ee69
Santcar05 <castrogarzonsantiago@gmail.com>
2025-08-15 22:50:57 -0500

    Uso de requestMapping

--- src/main/java/com/example/demo/controller/HolaMundoController.java
+++ src/main/java/com/example/demo/controller/HolaMundoController.java
@@ -2,9 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+    //Buena practica:  utilizar requestMapping en vez de colocar la url en el getMapping
 @Controller
+@RequestMapping("/mundo")
 public class HolaMundoController {
+
     //Que tipo de peticion se estará haciendo
     @GetMapping("/hola")
     //@ResponseBody //Retornar codigo de html en la función(NO UTILIZAR CUANDO SE UTILIZA THYMELEAF)
@@ -12,11 +16,12 @@ public class HolaMundoController {
         return "holaMundo";//Sin la extension
     }
 
-
-    @GetMapping("/chao")
+    @GetMapping("chao")
     public String chaoMundo() {
         return "chaoMundo";
     }
+
+    //Solo puede haber una sola peticion get por url
 
 }
 
